6570d133deb8af591f95be7d9bc4c5de3748cf6c
Abbos <norqulovabbos1701@gmail.com>
2024-03-30 23:25:10 +0500

    red

--- src/pages/Question.jsx
+++ src/pages/Question.jsx
@@ -6,6 +6,41 @@ const Question = () => {
     // const {name,surname} = useSelector()
     let [index,setIndex] = useState(1)
     let [question,setQuestion] = useState(data[index-1])
+
+    let Option1 = useRef(null)
+    let Option2 = useRef(null)
+    let Option3 = useRef(null)
+    let Option4 = useRef(null)
+
+    let option_arr = [Option1,Option2,Option3,Option4]
+
+
+
+const Next = () => {
+    if(data.length === index){
+        // const text = `%0A 👦 Name: ${name}  %0A 👦 Surname:  ${surname} %0A  ✅Score:  ${score} / ${data.length}  %0A ⏳timer:  ${Math.floor(seconds / 60)}:${seconds % 60}`;
+        // const chatId = -1002128588085;
+        // const token = '6834109969:AAEhUkHL4MsMs8Be2CWGY9oC7KXSbW8JHAM';
+        // const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${text}&parse_mode=html`;
+        // let api = new XMLHttpRequest();
+        // api.open("GET", url, true);
+        // api.send();
+    }
+    else{
+        // setIndex(prev=>prev+1)
+            setIndex(++index)
+            setQuestion(data[index-1])
+            option_arr.map(option=>{
+                option.current.classList.remove("correct")
+                option.current.classList.remove("wrong")
+                return 0;
+            })
+    }
+}
+
+
+
+
     return (
         <div className='w-full h-[100vh] bg-[#5cc2eb] containerr '>
                     <div className='relative w-full h-[100vh]'>
@@ -15,37 +50,37 @@ const Question = () => {
                                 <>
                                 <div className='p-3 font-bold text-[30px] text-[red]'>
     
-            {/* <h1>2:34</h1>
-            <h1>Timer finished!</h1> */}
+            <h1>10:00</h1>
+            <h1>Timer finished!</h1>
           
                                 </div>
-                                {/* <div className='flex justify-center font-bold text-[20px] text-[black]'>
+                                <div className='flex justify-center font-bold text-[20px] text-[black]'>
                                     <div className='flex flex-col items-start'>
                                         <h1>Name :</h1>
                                         <h1>Surname :</h1>
                                     </div>
                                     <div className='flex flex-col items-start ml-4'>
-                                        <p>Abbos{name}</p>
-                                        <p>Norqulov{surname}</p>
+                                        <p>Abbos</p>
+                                        <p>Norqulov</p>
                                     </div>
-                                </div> */}
+                                </div>
                                 <div className='flex flex-col gap-3 py-10 mt-4  bg-[#cb9595] rounded-[20px]'>
                                     <div className='flex gap-1 px-2 font-medium text-[25px]'>
                                         <h1 className=''>Question</h1>
                                         <p>{index}</p>/<p className='text-[26px]'>{data.length}</p>
                                     </div>
                                     <div className='flex p-2 bg-green'>
-                                        <h2 className=' text-start  w-[60%]'>1.saolllards</h2>
+                                        <h2 className=' text-start  w-[60%]'>{index}.saolllards</h2>
                                         
                                             <div className='flex flex-col justify-start w-[200px]  '>
-                                                <p className='border-[4px] border-solid border-[#00f7ff] rounded-[20px] mb-4 bg-[white] text-[black] cursor-pointer'    >xcs</p>
-                                                <p className='border-[4px] border-solid border-[#00f7ff] rounded-[20px] mb-4 bg-[white] text-[black] cursor-pointer'   >toshkent</p>
-                                                <p className='border-[4px] border-solid border-[#00f7ff] rounded-[20px] mb-4 bg-[white] text-[black] cursor-pointer'    >toshkent</p>
-                                                <p className='border-[4px] border-solid border-[#00f7ff] rounded-[20px] mb-4 bg-[white] text-[black] cursor-pointer'    >toshkent</p>
+                                                <p className='border-[4px] border-solid border-[#00f7ff] rounded-[20px] mb-4 bg-[white] text-[black] cursor-pointer' ref={Option1}   >{question.option1}</p>
+                                                <p className='border-[4px] border-solid border-[#00f7ff] rounded-[20px] mb-4 bg-[white] text-[black] cursor-pointer' ref={Option2}   >{question.option2}</p>
+                                                <p className='border-[4px] border-solid border-[#00f7ff] rounded-[20px] mb-4 bg-[white] text-[black] cursor-pointer' ref={Option3}   >{question.option3}</p>
+                                                <p className='border-[4px] border-solid border-[#00f7ff] rounded-[20px] mb-4 bg-[white] text-[black] cursor-pointer' ref={Option4}   >{question.option4}</p>
                                             </div>
                                     </div>
                                 </div>
-                                <p className='text-[white] font-bold bg-[green] text-[30px] mt-[20px] w-[200px] rounded-[20px] cursor-pointer' >Next</p>
+                                <p className='text-[white] font-bold bg-[green] text-[30px] mt-[20px] w-[200px] rounded-[20px] cursor-pointer' onClick={Next}>Next</p>
                                     </>
                                     
                                     <div className='flex justify-center gap-5'>
