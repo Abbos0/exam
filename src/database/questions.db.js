4f0618a2c6ce299a3a73e306f6ef7c0ab6535278
Abbos <norqulovabbos1701@gmail.com>
2024-06-19 23:16:59 +0500

    red

--- src/database/questions.db.js
+++ src/database/questions.db.js
@@ -1,83 +1,208 @@
+// export const data = [
+     
+//     {
+//         question: "Which continent has the highest number of countries?",
+//         option1: "Asia",
+//         option2: "Europe",
+//         option3: "North America",
+//         option4: "Africa",
+//         ans: 4,
+//       },
+//       {
+//         question: "Which continent has the highest number of countries?",
+//         option1: "Asia",
+//         option2: "Europe",
+//         option3: "North America",
+//         option4: "Africa",
+//         ans: 4,
+//       },
+//       {
+//         question: "Junk e-mail is also called?",
+//         option1: "Spam",
+//         option2: "Fake",
+//         option3: "Archived",
+//         option4: "Bin",
+//         ans: 1,
+//       },
+//       {
+//         question: "A computer cannot BOOT if it does not have the?",
+//         option1: "Application Software",
+//         option2: "Internet",
+//         option3: "Operating System",
+//         option4: "Mouse",
+//         ans: 3,
+//       },
+//       {
+//         question: "First page of Website is termed as?",
+//         option1: "Index Page",
+//         option2: "Homepage",
+//         option3: "Sitemap",
+//         option4: "Pen Drive",
+//         ans: 2,
+//       },
+//        {
+//         question: "A computer cannot BOOT if it does not have the?",
+//         option1: "Application Software",
+//         option2: "Internet",
+//         option3: "Operating System",
+//         option4: "Mouse",
+//         ans: 3,
+//       },
+//       {
+//         question: "First page of Website is termed as?",
+//         option1: "Index Page",
+//         option2: "Homepage",
+//         option3: "Sitemap",
+//         option4: "Pen Drive",
+//         ans: 2,
+//       },
+//       {
+//           question: "Which continent has the highest number of countries?",
+//           option1: "Asia",
+//           option2: "Europe",
+//           option3: "North America",
+//           option4: "Africa",
+//           ans: 4,
+//         },
+//         {
+//           question: "Junk e-mail is also called?",
+//           option1: "Spam",
+//           option2: "Fake",
+//           option3: "Archived",
+//           option4: "Bin",
+//           ans: 1,
+//         },
+//         {
+//           question: "A computer cannot BOOT if it does not have the?",
+//           option1: "Application Software",
+//           option2: "Internet",
+//           option3: "Operating System",
+//           option4: "Mouse",
+//           ans: 3,
+//         },
+//     ];
+
+
+
+
+
+
+
+
+
 export const data = [
      
-    {
-        question: "Which continent has the highest number of countries?",
-        option1: "Asia",
-        option2: "Europe",
-        option3: "North America",
-        option4: "Africa",
+   
+      {
+        question: "KES va IEM lar bir – biridan farqlanishi nimada?",
+        option1: "kes da fakat issiqlik energiya, iem da esa elektr energiya ham issiqlik energiya ishlab chiqariladi ",
+        option2: "kes da kimyoviy energiya, iem da esa elektr energiya ishlab chiqariladi ",
+        option3: "kes da elektr energiya, iem da esa issiqlik energiya ishlab chiqariladi ",
+        option4: "kes da fakat elektr energiya, iem da esa elektr energiya ham issiqlik energiya ishlab chiqariladi",
+        ans: 4,
+      }, 
+      {
+        question: "Qozon agregati deb nimaga aytiladi?",
+        option1: "Bug’ ta’minlovchi uskuna",
+        option2: " Suv qizdiruvchi agregat",
+        option3: " Suv ta’minlab beruvchi qurilma",
+        option4: " Suv bug’ini ishlab chiqaruvchi qurilma",
         ans: 4,
       },
       {
-        question: "Which continent has the highest number of countries?",
-        option1: "Asia",
-        option2: "Europe",
-        option3: "North America",
-        option4: "Africa",
-        ans: 4,
-      },
-      {
-        question: "Junk e-mail is also called?",
-        option1: "Spam",
-        option2: "Fake",
-        option3: "Archived",
-        option4: "Bin",
+        question: "Suv isitish qozoni deb nimaga aytiladi?",
+        option1: " Issiq suv ishlab chiqaruvchi qurilma",
+        option2: "  Issiq suv va bug’ islab chiqaruvchi qurilma",
+        option3: " Bug’ ishlab chiqaruvchi qurilma",
+        option4: " Suvni qizdirib bug’ga aylantiruvchi qurilma",
         ans: 1,
       },
       {
-        question: "A computer cannot BOOT if it does not have the?",
-        option1: "Application Software",
-        option2: "Internet",
-        option3: "Operating System",
-        option4: "Mouse",
+        question: "Qozon qurilmasi deb nimaga aytiladi?",
+        option1:  " Suvni yuqori haroratda yetkazib beradi",
+        option2:" Issiq suv va suv bug’i  ishlab chiqaruvchi qurilma va yordamchi jihozlar",
+        option3: "Yoqilg’idan yuqori harorat oladi ",
+        option4: " Ishchi jismi gaz bo’lgan qurilma",
+        ans: 2,
+      }, 
+      {
+        question: "Bug’ generatorini asosiy qismlari?",
+        option1:"ekonomayzer, havo qizdirgich va past bosimli silindr",
+        option2:"bug’ qizdirgich, ekonomayzer va kondensator",
+        option3:"o’txona, bug’ qizdirgich, ekonomayzer va havo qizdirgich",
+        option4:"havo qizdirgich va ekonomayzer",
         ans: 3,
       },
       {
-        question: "First page of Website is termed as?",
-        option1: "Index Page",
-        option2: "Homepage",
-        option3: "Sitemap",
-        option4: "Pen Drive",
-        ans: 2,
-      },
-       {
-        question: "A computer cannot BOOT if it does not have the?",
-        option1: "Application Software",
-        option2: "Internet",
-        option3: "Operating System",
-        option4: "Mouse",
-        ans: 3,
+        question: "Qanday sirkulyasiyali qozonlar mavjud?",
+        option1:"Tabiiy, majburiy, to’g’ri oqimli.",
+        option2:"Tabiiy, majburiy, teskari oqimli.",
+        option3:"To’g’ri oqimli, majburiy, to’ri oqimli, teskari to’g’ri oqimli.",
+        option4:"To’g’ri oqimli, teskari oqimli, aralash oqimli.",
+        ans: 1,
       },
       {
-        question: "First page of Website is termed as?",
-        option1: "Index Page",
-        option2: "Homepage",
-        option3: "Sitemap",
-        option4: "Pen Drive",
-        ans: 2,
+        question: "Qanday sirkulyasiyali qozonlar mavjud?",
+        option1: "Tabiiy, majburiy, to’g’ri oqimli.",
+        option2: "Tabiiy, majburiy, teskari oqimli.",
+        option3: "To’g’ri oqimli, majburiy, to’ri oqimli, teskari to’g’ri oqimli.",
+        option4: "To’g’ri oqimli, teskari oqimli, aralash oqimli.",
+        ans: 1
       },
       {
-          question: "Which continent has the highest number of countries?",
-          option1: "Asia",
-          option2: "Europe",
-          option3: "North America",
-          option4: "Africa",
-          ans: 4,
-        },
-        {
-          question: "Junk e-mail is also called?",
-          option1: "Spam",
-          option2: "Fake",
-          option3: "Archived",
-          option4: "Bin",
-          ans: 1,
-        },
-        {
-          question: "A computer cannot BOOT if it does not have the?",
-          option1: "Application Software",
-          option2: "Internet",
-          option3: "Operating System",
-          option4: "Mouse",
-          ans: 3,
-        },
-    ];
+        question: "Sirkulyasiya karraligi deb nimaga aytiladi?",
+        option1: "Suv massa birligining bug’ga aylanish qiymati.",
+        option2: "Bug’ning suvga aylanishi",
+        option3: "Suvning qozonda aylanishi",
+        option4: "Bug’ va suvning aylanish tezligi.",
+        ans: 1
+      },
+      {
+        question: "Tabiiy sirkulyasiyali qozonlarda sirkulyasiya karraligi nechaga teng?",
+        option1: "K=4-30",
+        option2: "K=3-40",
+        option3: "K=3-10",
+        option4: "K=1",
+        ans: 1
+      },
+      {
+        question: "Majburiy sirkulyasiyali qozonlarda sirkulyasiya karraligi nechaga teng?",
+        option1: "K=3-10",
+        option2: "K=4-30",
+        option3: "K=1",
+        option4: "K=3-40",
+        ans: 1
+      },
+      {
+        question: "To’g’ri oqimli qozonlarda sirkulyasiya karraligi nechaga teng?",
+        option1: "K=1",
+        option2: "K=3-40",
+        option3: "K=3-10",
+        option4: "K=4-30",
+        ans: 1
+      },
+      {
+        question: "Tutun so’rgichning vazifasi?",
+        option1: "Bug’ generatori o’txonasidan yonish mahsulotlarini chiqarish.",
+        option2: "havoni o’choqqa uzatish.",
+        option3: "havo sirkulyasiyasini me'yorlash.",
+        option4: "Bug’ haroratini rostlash.",
+        ans: 1
+      },
+      {
+        question: "Bug’ haroratini rostlash?",
+        option1: "O’txonada joylashgan yuzalar",
+        option2: "gorizontal gaz yo’lida joylashgan yuzalar",
+        option3: "konvektiv shaxtada joylashgan yuzalar",
+        option4: "o’txonani tashqi devorida joylashgan yuzalar",
+        ans: 1
+      },
+      {
+        question: "Bug’ bosimiga ko’ra qozonlar necha turga bo’linadi?",
+        option1: "4 ga",
+        option2: "3ga",
+        option3: "2ga",
+        option4: "5ga",
+        ans: 1
+      },
+  ];
