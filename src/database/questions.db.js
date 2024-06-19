48047e37e42e78687c53790cf69c1e26ec7d52ff
Abbos <norqulovabbos1701@gmail.com>
2024-06-19 23:36:44 +0500

    ererefe

--- src/database/questions.db.js
+++ src/database/questions.db.js
@@ -101,108 +101,108 @@ export const data = [
         option4: "kes da fakat elektr energiya, iem da esa elektr energiya ham issiqlik energiya ishlab chiqariladi",
         ans: 4,
       }, 
-      // {
-      //   question: "Qozon agregati deb nimaga aytiladi?",
-      //   option1: "Bug’ ta’minlovchi uskuna",
-      //   option2: " Suv qizdiruvchi agregat",
-      //   option3: " Suv ta’minlab beruvchi qurilma",
-      //   option4: " Suv bug’ini ishlab chiqaruvchi qurilma",
-      //   ans: 4,
-      // },
-      // {
-      //   question: "Suv isitish qozoni deb nimaga aytiladi?",
-      //   option1: " Issiq suv ishlab chiqaruvchi qurilma",
-      //   option2: "  Issiq suv va bug’ islab chiqaruvchi qurilma",
-      //   option3: " Bug’ ishlab chiqaruvchi qurilma",
-      //   option4: " Suvni qizdirib bug’ga aylantiruvchi qurilma",
-      //   ans: 1,
-      // },
-      // {
-      //   question: "Qozon qurilmasi deb nimaga aytiladi?",
-      //   option1:  " Suvni yuqori haroratda yetkazib beradi",
-      //   option2:" Issiq suv va suv bug’i  ishlab chiqaruvchi qurilma va yordamchi jihozlar",
-      //   option3: "Yoqilg’idan yuqori harorat oladi ",
-      //   option4: " Ishchi jismi gaz bo’lgan qurilma",
-      //   ans: 2,
-      // }, 
-      // {
-      //   question: "Bug’ generatorini asosiy qismlari?",
-      //   option1:"ekonomayzer, havo qizdirgich va past bosimli silindr",
-      //   option2:"bug’ qizdirgich, ekonomayzer va kondensator",
-      //   option3:"o’txona, bug’ qizdirgich, ekonomayzer va havo qizdirgich",
-      //   option4:"havo qizdirgich va ekonomayzer",
-      //   ans: 3,
-      // },
-      // {
-      //   question: "Qanday sirkulyasiyali qozonlar mavjud?",
-      //   option1:"Tabiiy, majburiy, to’g’ri oqimli.",
-      //   option2:"Tabiiy, majburiy, teskari oqimli.",
-      //   option3:"To’g’ri oqimli, majburiy, to’ri oqimli, teskari to’g’ri oqimli.",
-      //   option4:"To’g’ri oqimli, teskari oqimli, aralash oqimli.",
-      //   ans: 1,
-      // },
-      // {
-      //   question: "Qanday sirkulyasiyali qozonlar mavjud?",
-      //   option1: "Tabiiy, majburiy, to’g’ri oqimli.",
-      //   option2: "Tabiiy, majburiy, teskari oqimli.",
-      //   option3: "To’g’ri oqimli, majburiy, to’ri oqimli, teskari to’g’ri oqimli.",
-      //   option4: "To’g’ri oqimli, teskari oqimli, aralash oqimli.",
-      //   ans: 1
-      // },
-      // {
-      //   question: "Sirkulyasiya karraligi deb nimaga aytiladi?",
-      //   option1: "Suv massa birligining bug’ga aylanish qiymati.",
-      //   option2: "Bug’ning suvga aylanishi",
-      //   option3: "Suvning qozonda aylanishi",
-      //   option4: "Bug’ va suvning aylanish tezligi.",
-      //   ans: 1
-      // },
-      // {
-      //   question: "Tabiiy sirkulyasiyali qozonlarda sirkulyasiya karraligi nechaga teng?",
-      //   option1: "K=4-30",
-      //   option2: "K=3-40",
-      //   option3: "K=3-10",
-      //   option4: "K=1",
-      //   ans: 1
-      // },
-      // {
-      //   question: "Majburiy sirkulyasiyali qozonlarda sirkulyasiya karraligi nechaga teng?",
-      //   option1: "K=3-10",
-      //   option2: "K=4-30",
-      //   option3: "K=1",
-      //   option4: "K=3-40",
-      //   ans: 1
-      // },
-      // {
-      //   question: "To’g’ri oqimli qozonlarda sirkulyasiya karraligi nechaga teng?",
-      //   option1: "K=1",
-      //   option2: "K=3-40",
-      //   option3: "K=3-10",
-      //   option4: "K=4-30",
-      //   ans: 1
-      // },
-      // {
-      //   question: "Tutun so’rgichning vazifasi?",
-      //   option1: "Bug’ generatori o’txonasidan yonish mahsulotlarini chiqarish.",
-      //   option2: "havoni o’choqqa uzatish.",
-      //   option3: "havo sirkulyasiyasini me'yorlash.",
-      //   option4: "Bug’ haroratini rostlash.",
-      //   ans: 1
-      // },
-      // {
-      //   question: "Bug’ haroratini rostlash?",
-      //   option1: "O’txonada joylashgan yuzalar",
-      //   option2: "gorizontal gaz yo’lida joylashgan yuzalar",
-      //   option3: "konvektiv shaxtada joylashgan yuzalar",
-      //   option4: "o’txonani tashqi devorida joylashgan yuzalar",
-      //   ans: 1
-      // },
-      // {
-      //   question: "Bug’ bosimiga ko’ra qozonlar necha turga bo’linadi?",
-      //   option1: "4 ga",
-      //   option2: "3ga",
-      //   option3: "2ga",
-      //   option4: "5ga",
-      //   ans: 1
-      // },
+      {
+        question: "Qozon agregati deb nimaga aytiladi?",
+        option1: "Bug’ ta’minlovchi uskuna",
+        option2: " Suv qizdiruvchi agregat",
+        option3: " Suv ta’minlab beruvchi qurilma",
+        option4: " Suv bug’ini ishlab chiqaruvchi qurilma",
+        ans: 4,
+      },
+      {
+        question: "Suv isitish qozoni deb nimaga aytiladi?",
+        option1: " Issiq suv ishlab chiqaruvchi qurilma",
+        option2: "  Issiq suv va bug’ islab chiqaruvchi qurilma",
+        option3: " Bug’ ishlab chiqaruvchi qurilma",
+        option4: " Suvni qizdirib bug’ga aylantiruvchi qurilma",
+        ans: 1,
+      },
+      {
+        question: "Qozon qurilmasi deb nimaga aytiladi?",
+        option1:  " Suvni yuqori haroratda yetkazib beradi",
+        option2:" Issiq suv va suv bug’i  ishlab chiqaruvchi qurilma va yordamchi jihozlar",
+        option3: "Yoqilg’idan yuqori harorat oladi ",
+        option4: " Ishchi jismi gaz bo’lgan qurilma",
+        ans: 2,
+      }, 
+      {
+        question: "Bug’ generatorini asosiy qismlari?",
+        option1:"ekonomayzer, havo qizdirgich va past bosimli silindr",
+        option2:"bug’ qizdirgich, ekonomayzer va kondensator",
+        option3:"o’txona, bug’ qizdirgich, ekonomayzer va havo qizdirgich",
+        option4:"havo qizdirgich va ekonomayzer",
+        ans: 3,
+      },
+      {
+        question: "Qanday sirkulyasiyali qozonlar mavjud?",
+        option1:"Tabiiy, majburiy, to’g’ri oqimli.",
+        option2:"Tabiiy, majburiy, teskari oqimli.",
+        option3:"To’g’ri oqimli, majburiy, to’ri oqimli, teskari to’g’ri oqimli.",
+        option4:"To’g’ri oqimli, teskari oqimli, aralash oqimli.",
+        ans: 1,
+      },
+      {
+        question: "Qanday sirkulyasiyali qozonlar mavjud?",
+        option1: "Tabiiy, majburiy, to’g’ri oqimli.",
+        option2: "Tabiiy, majburiy, teskari oqimli.",
+        option3: "To’g’ri oqimli, majburiy, to’ri oqimli, teskari to’g’ri oqimli.",
+        option4: "To’g’ri oqimli, teskari oqimli, aralash oqimli.",
+        ans: 1
+      },
+      {
+        question: "Sirkulyasiya karraligi deb nimaga aytiladi?",
+        option1: "Suv massa birligining bug’ga aylanish qiymati.",
+        option2: "Bug’ning suvga aylanishi",
+        option3: "Suvning qozonda aylanishi",
+        option4: "Bug’ va suvning aylanish tezligi.",
+        ans: 1
+      },
+      {
+        question: "Tabiiy sirkulyasiyali qozonlarda sirkulyasiya karraligi nechaga teng?",
+        option1: "K=4-30",
+        option2: "K=3-40",
+        option3: "K=3-10",
+        option4: "K=1",
+        ans: 1
+      },
+      {
+        question: "Majburiy sirkulyasiyali qozonlarda sirkulyasiya karraligi nechaga teng?",
+        option1: "K=3-10",
+        option2: "K=4-30",
+        option3: "K=1",
+        option4: "K=3-40",
+        ans: 1
+      },
+      {
+        question: "To’g’ri oqimli qozonlarda sirkulyasiya karraligi nechaga teng?",
+        option1: "K=1",
+        option2: "K=3-40",
+        option3: "K=3-10",
+        option4: "K=4-30",
+        ans: 1
+      },
+      {
+        question: "Tutun so’rgichning vazifasi?",
+        option1: "Bug’ generatori o’txonasidan yonish mahsulotlarini chiqarish.",
+        option2: "havoni o’choqqa uzatish.",
+        option3: "havo sirkulyasiyasini me'yorlash.",
+        option4: "Bug’ haroratini rostlash.",
+        ans: 1
+      },
+      {
+        question: "Bug’ haroratini rostlash?",
+        option1: "O’txonada joylashgan yuzalar",
+        option2: "gorizontal gaz yo’lida joylashgan yuzalar",
+        option3: "konvektiv shaxtada joylashgan yuzalar",
+        option4: "o’txonani tashqi devorida joylashgan yuzalar",
+        ans: 1
+      },
+      {
+        question: "Bug’ bosimiga ko’ra qozonlar necha turga bo’linadi?",
+        option1: "4 ga",
+        option2: "3ga",
+        option3: "2ga",
+        option4: "5ga",
+        ans: 1
+      },
   ];
